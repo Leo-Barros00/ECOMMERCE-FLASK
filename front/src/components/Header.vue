936dc5eb5cc3f660e36c94fb21a6426008346c87
Filipe Neves <filipe.nevser@gmail.com>
2023-08-19 01:18:31 -0300

    feat: add new header component

---
+++ front/src/components/Header.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { minicart } from '../store';
+</script>
+
+<template>
+  <header class="flex flex-col">
+    <div
+      class="flex justify-between items-center py-5 bg-white max-w-container w-container mx-auto"
+    >
+      <span class="font-bold">FlasKommerce</span>
+      <button class="p-2" @click="minicart.toggleOpen">
+        <img src="../assets/basket.svg" alt="Carrinho" />
+      </button>
+    </div>
+    <div class="flex py-2 bg-emerald-300">
+      <div class="max-w-container w-container mx-auto">categories</div>
+    </div>
+  </header>
+</template>
