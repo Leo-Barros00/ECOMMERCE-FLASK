1d48c450af4eca0d29c66668e939f092bbb56520
Filipe Neves <filipe.nevser@gmail.com>
2023-08-19 01:17:21 -0300

    feat: add minicart component

---
+++ front/src/components/Minicart.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { minicart } from '../store';
+</script>
+
+<template>
+  <div
+    class="absolute w-full h-full left-0 top-0 bg-black duration-100"
+    :class="{
+      'pointer-events-none': !minicart.isOpen,
+      'opacity-0': !minicart.isOpen,
+      'opacity-20': minicart.isOpen
+    }"
+    @click="minicart.toggleOpen"
+  ></div>
+  <div
+    class="fixed left-full top-0 h-full z-10 duration-100 bg-slate-50 p-4"
+    :class="{ '-translate-x-full': minicart.isOpen }"
+  >
+    aa
+  </div>
+</template>
---
+++ front/src/store/index.ts
@@ -0,0 +1,1 @@
+export { default as minicart } from "./minicart";
