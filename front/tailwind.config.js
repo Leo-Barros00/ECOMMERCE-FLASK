a7d85dde92fa60a0b4e875c09304fa3d1d29a42e
Filipe Neves <filipe.nevser@gmail.com>
2023-08-23 22:18:49 -0300

    config: add custom values to tailwind configuration

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -1,13 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {
       width: {
-        container: "92.5vw"
+        container: '92.5vw'
       },
       maxWidth: {
-        container: "1280px"
+        container: '1280px'
+      },
+      colors: {
+        primary: '#ef233c',
+        secondary: '#2b2d42'
       }
     }
   },
