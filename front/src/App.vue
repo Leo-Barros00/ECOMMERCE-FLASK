fb5951bb1ef3ab5167c2c9cc13077a05d59cc9b0
Filipe Neves <filipe.nevser@gmail.com>
2023-08-23 22:18:11 -0300

    fix: remove default layout from app root

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,10 +1,3 @@
-<script setup lang="ts">
-import Header from './components/Header.vue';
-import Minicart from './components/Minicart.vue';
-</script>
-
 <template>
-  <Header />
-  <Minicart />
   <router-view></router-view>
 </template>
