c5b170c6b777c57ce33fad78306d1aa7633b6d4b
Filipe Neves <filipe.nevser@gmail.com>
2023-08-23 22:19:46 -0300

    feat: add new route configuration to divide environment

--- front/src/router.ts
+++ front/src/router.ts
@@ -1,7 +1,33 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { RouteRecordRaw, createRouter, createWebHistory } from 'vue-router';
+import DefaultLayout from './layouts/DefaultLayout.vue';
+import AdminLayout from './layouts/AdminLayout.vue';
+
 import Home from './pages/Home.vue';
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    component: DefaultLayout,
+    children: [
+      {
+        path: '',
+        component: Home
+      }
+    ]
+  },
+  {
+    path: '/admin',
+    component: AdminLayout,
+    children: [
+      {
+        path: '',
+        component: Home
+      }
+    ]
+  }
+];
 
 export const router = createRouter({
   history: createWebHistory(),
-  routes: [{ path: '/', component: Home }]
+  routes
 });
