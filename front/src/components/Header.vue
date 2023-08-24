23af078bc59be25cbe1de5566cf5647c9580bcb0
Filipe Neves <filipe.nevser@gmail.com>
2023-08-23 23:12:09 -0300

    fix: header layout

--- front/src/components/Header.vue
+++ front/src/components/Header.vue
@@ -1,14 +1,17 @@
 <script setup lang="ts">
 import { minicart } from '../store';
 import BasketIcon from './BasketIcon.vue';
+import MenuIcon from './MenuIcon.vue';
 </script>
 
 <template>
-  <header class="flex flex-col">
+  <header class="flex flex-col bg-primary">
     <div
-      class="flex justify-between items-center py-5 bg-white max-w-container w-container mx-auto"
+      class="flex justify-between items-center py-5 text-white max-w-container w-container mx-auto"
     >
-      <span class="font-bold">FlasKommerce</span>
+      <router-link to="/"
+        ><span class="font-bold text-2xl">FlasKommerce</span></router-link
+      >
       <button
         class="p-2 hover:scale-125 duration-100"
         @click="minicart.toggleOpen"
@@ -16,8 +19,17 @@ import BasketIcon from './BasketIcon.vue';
         <basket-icon class="fill-white" />
       </button>
     </div>
-    <div class="flex py-2 bg-emerald-300">
-      <div class="max-w-container w-container mx-auto">categories</div>
+    <div class="flex bg-secondary text-white text-lg font-medium">
+      <div
+        class="max-w-container w-container mx-auto flex justify-between flex-wrap gap-x-8 relative"
+      >
+        <router-link to="/categories"
+          ><span class="group flex gap-3 py-3 items-center">
+            <menu-icon class="fill-white" />
+            <span>Todas as categorias</span>
+          </span></router-link
+        >
+      </div>
     </div>
   </header>
 </template>
