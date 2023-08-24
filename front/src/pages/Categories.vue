bbec274516743e23590c985870ed85b2c7ba9dc2
Filipe Zanelato <filipe_zanelato@hotmail.com>
2023-08-24 20:25:29 -0300

    feat: add fetch categories to page

--- front/src/pages/Categories.vue
+++ front/src/pages/Categories.vue
@@ -5,5 +5,42 @@ import PageTitle from '../components/PageTitle.vue';
 <template>
   <div class="max-w-container w-container mx-auto my-6">
     <PageTitle>Categorias</PageTitle>
+    <h2>{{ categories }}</h2>
   </div>
 </template>
+
+<script lang="ts">
+export default {
+  name: 'Categories',
+  data() {
+    return {
+      categories: []
+    };
+  },
+  methods: {
+    async getCategories() {
+      try {
+        const response = await fetch('http://127.0.0.1:5000/categories', {
+          method: 'GET',
+          headers: {
+            accept: 'application/json'
+          }
+        });
+
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+
+        const data = await response.json();
+        this.categories = data;
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  },
+
+  async created() {
+    await this.getCategories();
+  }
+};
+</script>
