36870ee05372201dfd2f3e46a2a7cbe2d582d5f7
Filipe Neves <filipe.nevser@gmail.com>
2023-08-24 20:13:59 -0300

    feat: add date default values to models

--- back/app/models.py
+++ back/app/models.py
@@ -1,5 +1,7 @@
-from app import db
+from sqlalchemy import func
 import uuid
+from app import db
+from app.utils.date import formatDateISO
 
 class Product(db.Model):
   id = db.Column(db.String, primary_key=True, default=lambda: str(uuid.uuid4()))
@@ -9,6 +11,8 @@ class Product(db.Model):
   image_url = db.Column(db.String(200), nullable=True)
   stock_quantity = db.Column(db.Integer, nullable=False)
   category_id = db.Column(db.String, db.ForeignKey('category.id'))
+  created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
+  updated_at = db.Column(db.DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
 
   def to_dict(self):
     return {
@@ -16,9 +20,11 @@ class Product(db.Model):
       'name': self.name,
       'description': self.description,
       'price': self.price,
-      'image_url': self.image_url,
-      'stock_quantity': self.stock_quantity,
-      'category': self.category_id
+      'imageUrl': self.image_url,
+      'stockQuantity': self.stock_quantity,
+      'category': self.category_id,
+      'createdAt': formatDateISO(self.created_at),
+      'updatedAt': formatDateISO(self.updated_at),
     }
   
 class Category(db.Model):
@@ -26,10 +32,26 @@ class Category(db.Model):
   name = db.Column(db.String(80), nullable=False)
   slug = db.Column(db.String(80), nullable=False)
   products = db.relationship('Product', backref='category')
+  created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
+  updated_at = db.Column(db.DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
  
   def to_dict(self):
     return {
       'id': self.id,
       'name': self.name,
-      'slug': self.slug
+      'slug': self.slug,
+      'createdAt': formatDateISO(self.created_at),
+      'updatedAt': formatDateISO(self.updated_at),
+    }
+  
+class Cart(db.Model):
+  id = db.Column(db.String, primary_key=True, default=lambda: str(uuid.uuid4()))
+  created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
+  updated_at = db.Column(db.DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
+
+  def to_dict(self):
+    return {
+      'id': self.id,
+      'createdAt': formatDateISO(self.created_at),
+      'updatedAt': formatDateISO(self.updated_at),
     }
