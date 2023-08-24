7c691c189267034aadaddb1edce00840cd9a3f3a
Filipe Neves <filipe.nevser@gmail.com>
2023-08-24 19:18:59 -0300

    feat: add categories and home page components

---
+++ front/src/pages/Home.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import PageTitle from '../components/PageTitle.vue';
+</script>
+
+<template>
+  <div class="max-w-container w-container mx-auto my-6">
+    <PageTitle>Página Inicial</PageTitle>
+  </div>
+</template>
