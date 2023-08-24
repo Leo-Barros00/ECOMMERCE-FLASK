a106ceb7f9bef7731307a6a0426504ec422aa37e
Filipe Neves <filipe.nevser@gmail.com>
2023-08-23 22:17:43 -0300

    feat: use layouts to divide environment

---
+++ front/src/layouts/DefaultLayout.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import Header from '../components/Header.vue';
+import Minicart from '../components/Minicart.vue';
+</script>
+
+<template>
+  <div>
+    <Header />
+    <Minicart />
+    <router-view></router-view>
+  </div>
+</template>
