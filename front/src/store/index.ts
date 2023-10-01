42c08c068d372f41433b92b025dcc5f95b912353
Filipe Neves <filipe.nevser@gmail.com>
2023-10-01 18:50:23 -0300

    feat: add more functions to minicart store

--- front/src/store/index.ts
+++ front/src/store/index.ts
@@ -1,1 +1,1 @@
-export { default as minicart } from "./minicart";
+export { default as minicart } from './minicart';
--- front/src/store/minicart.ts
+++ front/src/store/minicart.ts
@@ -4,5 +4,27 @@ export default reactive({
   isOpen: false,
   toggleOpen() {
     this.isOpen = !this.isOpen;
+  },
+  products: [] as any[],
+  address: '',
+  email: '',
+  addProduct(product: any) {
+    console.log({ product });
+    const existingProduct = this.products.find(({ id }) => id === product.id);
+
+    if (existingProduct) {
+      existingProduct.quantity += 1;
+    } else {
+      this.products.push({
+        ...product,
+        quantity: 1
+      });
+    }
+  },
+  removeProduct(productId: string) {
+    this.products = this.products.filter(({ id }) => productId !== id);
+  },
+  removeAllProducts() {
+    this.products = [];
   }
 });
